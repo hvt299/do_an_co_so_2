95c46095c7e0c7b3de01ee6482b1482bf5390c28
Thai Hua <huavietthai299@gmail.com>
2023-11-23 11:58:16 +0700

    Xây dựng giao diện dashboard (admin)

--- admin.php
+++ admin.php
@@ -82,16 +82,111 @@
         <div class="card--container">
             <h3 class="main--title">Today's data</h3>
             <div class="card--wrapper">
-                <div class="payment--card">
+                <div class="payment--card light-red">
                     <div class="card--header">
                         <div class="amount">
                             <span class="title">Payment amount</span>
                             <span class="amount--value">$500.00</span>
                         </div>
-                        <i class="fas fa-dollar-sign icon"></i>
+                        <i class="fas fa-dollar-sign icon dark-red"></i>
                     </div>
                     <span class="card-detail">**** **** **** 3484</span>
                 </div>
+
+                <div class="payment--card light-purple">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">Payment order</span>
+                            <span class="amount--value">$200.00</span>
+                        </div>
+                        <i class="fas fa-list icon dark-purple"></i>
+                    </div>
+                    <span class="card-detail">**** **** **** 5542</span>
+                </div>
+
+                <div class="payment--card light-green">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">Payment amount</span>
+                            <span class="amount--value">$350.00</span>
+                        </div>
+                        <i class="fas fa-users icon dark-green"></i>
+                    </div>
+                    <span class="card-detail">**** **** **** 8896</span>
+                </div>
+
+                <div class="payment--card light-blue">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">Payment amount</span>
+                            <span class="amount--value">$150.00</span>
+                        </div>
+                        <i class="fa-solid fa-check icon dark-blue"></i>
+                    </div>
+                    <span class="card-detail">**** **** **** 7745</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="tabular--wrapper">
+            <h3 class="main--title">Finance data</h3>
+            <div class="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Transaction Type</th>
+                            <th>Description</th>
+                            <th>Amount</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2023-05-01</td>
+                            <td>Expenses</td>
+                            <td>Office Supplies</td>
+                            <td>$250</td>
+                            <td>Office Expenses</td>
+                            <td>Pending</td>
+                            <td>
+                                <button class="edit">Edit</button>
+                                <button class="delete">Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2023-05-02</td>
+                            <td>Income</td>
+                            <td>Client Payment</td>
+                            <td>$500</td>
+                            <td>Sales</td>
+                            <td>Completed</td>
+                            <td>
+                                <button class="edit">Edit</button>
+                                <button class="delete">Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2023-05-03</td>
+                            <td>Expense</td>
+                            <td>Travel Expenses</td>
+                            <td>$250</td>
+                            <td>Travel</td>
+                            <td>Pending</td>
+                            <td>
+                                <button class="edit">Edit</button>
+                                <button class="delete">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="7">Total: $1,000</td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
     </div>
