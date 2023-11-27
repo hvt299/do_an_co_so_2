d26652cdedff7e556fbb4942650da21795fdddf5
Thai Hua <huavietthai299@gmail.com>
2023-11-27 23:57:57 +0700

    Xây dựng giao diện admin + fix lỗi đăng nhập admin (hàm get_account)

--- login_process.php
+++ login_process.php
@@ -12,19 +12,30 @@
         if (!empty($email) && !empty($password)){
             $account = get_account_by_email_password($email, $password);
             if (empty($account)) {
-                echo "<script>alert('Login Failed.'); location.href='login.php';</script>";
-                // header("Location: login.php");
+                $account = get_account_by_email_password_2($email, $password);
+                if (empty($account)) {
+                    echo "<script>alert('Login Failed.'); location.href='login.php';</script>";
+                    // header("Location: login.php");
+                } else {
+                    foreach($account as $acc){
+                        if ($acc['VaiTro'] == "Quản lý") {                        
+                            $_SESSION['username'] = $acc['Name'];
+                            $_SESSION['vaitro'] = $acc['VaiTro'];
+                            $_SESSION['matkhauungdung'] = $acc['MatKhauUngDung'];
+                            setcookie("username", $_SESSION['username'], time() + 3600);
+                            setcookie("vaitro", $_SESSION['vaitro'], time() + 3600);
+                            setcookie("matkhauungdung", $_SESSION['matkhauungdung'], time() + 3600);
+                            echo "<script>alert('Đăng nhập thành công!'); location.href='admin/index.php'; </script>";                        
+                        }
+                    }
+                }
             } else {
                 foreach($account as $acc){
-                    if ($acc['VaiTro'] == "Quản lý") {
-                        header("Location: admin.php");
-                    } else {
-                        // $_SESSION['email'] = $acc['email'];
+                    if ($acc['VaiTro'] == "Học viên") {
                         $_SESSION['idhv'] = $acc['IDHV']; // Đổi tên trường này
                         $_SESSION['username'] = $acc['Name'];
                         $_SESSION['vaitro'] = $acc['VaiTro'];
                         $_SESSION['matkhauungdung'] = $acc['MatKhauUngDung'];
-                        // setcookie("email", $_SESSION['email'], time() + 3600);
                         setcookie("idhv", $_SESSION['idhv'], time() + 3600); // Đổi tên trường này
                         setcookie("username", $_SESSION['username'], time() + 3600);
                         setcookie("vaitro", $_SESSION['vaitro'], time() + 3600);
--- model/identify_db.php
+++ model/identify_db.php
@@ -1,16 +1,4 @@
 <?php
-    // function get_account_by_email_password($email, $password) {
-    //     global $db;
-    //     $query = 'SELECT * FROM taikhoan
-    //               WHERE Email = :email AND Password = :password';
-    //     $statement = $db->prepare($query);
-    //     $statement->bindValue(":email", $email);
-    //     $statement->bindValue(":password", $password);
-    //     $statement->execute();
-    //     $account = $statement->fetchAll();
-    //     $statement->closeCursor();
-    //     return $account;
-    // }
     // sửa đổi
     function get_account_by_email_password($email, $password) {
         global $db;
@@ -18,6 +6,19 @@
                   FROM taikhoan
                   JOIN hocvien ON taikhoan.email = hocvien.email
                   WHERE taikhoan.Email = :email AND taikhoan.Password = :password';
+        $statement = $db->prepare($query);
+        $statement->bindValue(":email", $email);
+        $statement->bindValue(":password", $password);
+        $statement->execute();
+        $account = $statement->fetchAll();
+        $statement->closeCursor();
+        return $account;
+    }
+
+    function get_account_by_email_password_2($email, $password) {
+        global $db;
+        $query = 'SELECT * FROM taikhoan
+                  WHERE Email = :email AND Password = :password';
         $statement = $db->prepare($query);
         $statement->bindValue(":email", $email);
         $statement->bindValue(":password", $password);
